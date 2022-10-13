d59a339ea3f47c3155a18b105ed2c2dc465665f6
Mauro Jelvez Bosco <mauro_jelvez@hotmail.com>
2022-10-12 21:41:57 -0300

    ejercicio 1 empezado

---
+++ Ejercicio 1- Clase Persona POO/src/App.java
@@ -0,0 +1,9 @@
+/*Inicializar un objeto de la clase Persona ejercicio 7 de la guía POO, a null y tratar de invocar el 
+método esMayorDeEdad() a través de ese objeto. Luego, englobe el código con una cláusula 
+try-catch para probar la nueva excepción que debe ser controlada. */
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
